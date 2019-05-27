dc03c053de9fd6beeb1c10f0aa069dfa5d5dca72
Maksym <maksis90@gmail.com>
2019-05-27 15:32:20 +0300

    add slick-slider in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,7 @@ var sources = {
 	],
 	js: [
 		'./node_modules/jquery/dist/jquery.min.js',
+        './node_modules/slick-carousel/slick/slick.min.js',
 		'./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 	]
 };
