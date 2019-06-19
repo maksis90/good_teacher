1f80b6382ed84b32ae98f5c164089a67bdb48d8e
Maksym <maksis90@gmail.com>
2019-06-19 21:52:13 +0300

    fix main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,5 @@
 'use strict';
 
 (function ($) {
-    $(document).ready(function () {
-        // Code
-        $('.slider').slick({
-            lazyLoad: 'ondemand',
-            slidesToShow: 3,
-            slidesToScroll: 1
-        });
-    });
+    console.log('works');
 })(jQuery);
