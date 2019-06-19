8479e50efe8a7e51baddba85c075e7af19313373
Maksym <maksis90@gmail.com>
2019-06-19 22:24:35 +0300

    fix main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,48 @@
 'use strict';
 
-(function ($) {
-    console.log('works');
+(function($) {
+    $(document).ready(function() {
+        //PROFILE___ difficulty custom set selector
+
+        $('.difficulty__item--select').click(function() {
+            $('#custom_num').focus();
+        });
+        $('#custom_num').focus(function() {
+            $('#custom').prop('checked', true);
+        });
+
+        //PROFILE___ input range validation
+        jQuery.validator.setDefaults({
+            debug: false,
+            success: "validOn"
+        });
+        $("#difficulty").validate({
+            rules: {
+                field: {
+                    required: true,
+                    range: [11, 21]
+                }
+            }
+        });
+
+        // Code
+        $('.friend__slider').slick({
+            lazyLoad: 'ondemand',
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            // responsive: [{
+            //         breakpoint: 768,
+            //         settings: {
+            //             arrows: false,
+            //             centerMode: true,
+            //             centerPadding: '40px',
+            //             slidesToShow: 3
+            //         }
+            //     }
+        });
+    });
 })(jQuery);
